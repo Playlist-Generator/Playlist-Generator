ea21ec08137a404adb275843868f5f5fe2df2956
D-Stoqnov <97990126+D-Stoqnov@users.noreply.github.com>
2023-11-16 18:14:41 +0200

    Add files via upload

---
+++ PlaylistGeneratorV1/src/main/java/com/example/playlistgeneratorv1/PlaylistGeneratorV1Application.java
@@ -0,0 +1,13 @@
+package com.example.playlistgeneratorv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistGeneratorV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaylistGeneratorV1Application.class, args);
+    }
+
+}
---
+++ PlaylistGeneratorV1/src/test/java/com/example/playlistgeneratorv1/PlaylistGeneratorV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playlistgeneratorv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistGeneratorV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
